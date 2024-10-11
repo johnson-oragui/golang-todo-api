a9d205a8083e92f2efe86a37d0facf42e4f9cb66
johnson-oragui <johnson.oragui@gmail.com>
2024-10-11 14:50:41 +0100

    feat: moved route handlers to a routeHandler package

--- main.go
+++ main.go
@@ -1,83 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
+	"github.com/johnson-oragui/golang-todo-api/routeHandler"
+	"github.com/johnson-oragui/golang-todo-api/utils"
 )
-
-type Response struct {
-	Message    string `json:"message"`
-	StatusCode int    `json:"status_code"`
-	Data       any    `json:"data"`
-}
-
-// root handler function
-func homeHandler(w http.ResponseWriter, req *http.Request) {
-	res := Response{
-		Message:    "Welcome to the golang todo page!",
-		StatusCode: 200,
-	}
-	if req.URL.Path != "/" {
-		http.NotFound(w, req)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(res); err != nil {
-		http.Error(w, "Unable to encode JSON", http.StatusInternalServerError)
-	}
-}
-
-// api homehandler
-func apiHomeHandler(w http.ResponseWriter, r *http.Request) {
-	response := Response{
-		Message:    "API Home page!",
-		StatusCode: 200,
-	}
-
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(response); err != nil {
-		http.Error(w, "Unable to encode JSON", http.StatusInternalServerError)
-	}
-}
 
 // myHandler sets the server routes
 func myHandler() http.Handler {
-	mux := http.NewServeMux()
+	router := http.NewServeMux()
+	
+	routeHandler := routeHandler.New()
 
 	// Define handlers
-	mux.HandleFunc("/", homeHandler) // root handler
-	mux.HandleFunc("/api/v1", apiHomeHandler)
-	return logginMiddleware(mux)
+	router.HandleFunc("/", routeHandler.HomeHandler) // root handler
+	router.HandleFunc("/api/v1/auth/register", routeHandler.HandleUsers)
+	return utils.LogginMiddleware(router)
 }
 
-// loggingMiddleware is a middleware that logs HTTP requests
-func logginMiddleware(next http.Handler) http.Handler {
-
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Content-Type", "applicaton/json")
-		logger := make(map[string]string)
-		header := make(map[string]string)
-
-		logger["Method"] = req.Method
-		logger["Path"] = req.URL.Path
-		logger["RemoteAddr"] = req.RemoteAddr
-
-		// Convert the header to a string format that can be logged
-		for key, values := range req.Header {
-			header[key] = fmt.Sprintf("%s", values)
-		}
-
-		// Log request details
-		log.Printf("{ method: %v, path: %v, User-Agent: %v}", logger["Method"], logger["Path"], header["User-Agent"])
-
-		// Call the next handler in the chain
-		next.ServeHTTP(w, req)
-	},
-	)
-}
 
 func main() {
 	server := &http.Server{
