b56413567a48d994e6af4cb20f4f630a4ecbaf97
johnson-oragui <johnson.oragui@gmail.com>
2024-10-12 19:03:20 +0100

    feat: added Todo schema

--- schema/schema.go
+++ schema/schema.go
@@ -1,30 +1,47 @@
 package schema
 
 type Response struct {
-	Message string `json:"message"`
-	StatusCode int `json:"status_code"`
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code"`
 }
 
 type UserSchemaInput struct {
-	Username string
-	Email string
-	Password string
+	Username  string
+	Email     string
+	FirstName string
+	LastName  string
+	Password  string
 }
 
 type UserBase struct {
-	ID int			`json:"id"`
-	Username string	`json:"username"`
-	FirstName string	`json:"first_name"`
-	LastName string	`json:"last_name"`
-	Email string	`json:"email"`
-	Password string	`json:"password"`
+	ID        int    `json:"id"`
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
 }
 
 type UserSchemaOutput struct {
 	Response Response
-	Data UserBase `json:"data"`
+	Data     UserBase `json:"data"`
 }
 
-type DataBase struct {
+type TodoSchema struct {
+	Todo   string `json:"todo"`
+	Status string `json:"status"`
+}
+
+type TodoResponse struct {
+	Response Response
+	Data     TodoSchema `json:"data"`
+}
+
+type UsersDataBase struct {
 	Users map[string]UserBase
+}
+
+type TodoDataBase struct {
+	Username string
+	Todos    map[string]TodoSchema
 }
