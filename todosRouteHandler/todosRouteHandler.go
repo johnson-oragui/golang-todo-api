83778bcbeab138c1d9f697f894fbe8ae4d2eefcb
johnson-oragui <johnson.oragui@gmail.com>
2024-10-17 00:13:32 +0100

    feat: added todo-delete-handler, update-todo-handler, get-todo-handler

--- todosRouteHandler/todosRouteHandler.go
+++ todosRouteHandler/todosRouteHandler.go
@@ -3,7 +3,9 @@ package todosRouteHandler
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 
@@ -19,9 +21,7 @@ func New() *Router {
 func (r *Router) HandleTodos(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodGet:
-		r.HandleGetTodos(w, req)
-	case http.MethodPost:
-		r.HandleCreateTodo(w, req)
+		r.HandleGetTodo(w, req)
 	case http.MethodPut:
 		r.HandleUpdateTodo(w, req)
 	case http.MethodDelete:
@@ -53,12 +53,22 @@ func (r *Router) HandleCreateTodo(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// check if user exists in the users database
+	_, exists := schema.Database.Users[username]
+	if !exists {
+		log.Println("user does not exists in the database", username)
+		http.Error(w, "user does not exists in the database", http.StatusForbidden)
+		return
+	}
+
 	// create a nil TodoSchema struct
-	todoInput := schema.TodoSchema{}
+	todoInput := schema.TodoSchema{
+		ID: rand.Intn(10000),
+	}
 
 	// save the request body to the nill struct
 	if err := json.NewDecoder(req.Body).Decode(&todoInput); err != nil {
-		log.Println("Error decoding json")
+		log.Println("Error decoding json", todoInput)
 		http.Error(w, "Invalid JSON", http.StatusUnsupportedMediaType)
 		return
 	}
@@ -88,6 +98,7 @@ func (r *Router) HandleCreateTodo(w http.ResponseWriter, req *http.Request) {
 		Data: todoInput,
 	}
 
+	w.Header().Add("Content-Type", "applicaton/json")
 	// return the payload
 	w.WriteHeader(http.StatusCreated)
 
@@ -97,6 +108,7 @@ func (r *Router) HandleCreateTodo(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// Fetch all Todos GET /api/v1/users/{username}/todos
 func (r *Router) HandleGetTodos(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 
@@ -117,7 +129,7 @@ func (r *Router) HandleGetTodos(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	todos := schema.TodosDataBase.User[username].AllTodos
+	todos, exists := schema.TodosDataBase.User[username]
 	if !exists {
 		log.Printf("username %v does not exist", username)
 		http.Error(w, "user does not have a todo entry yet", http.StatusBadRequest)
@@ -129,9 +141,10 @@ func (r *Router) HandleGetTodos(w http.ResponseWriter, req *http.Request) {
 			Message:    "Todos retrieved successfully",
 			StatusCode: 200,
 		},
-		Data: todos,
+		Data: todos.AllTodos,
 	}
 
+	w.Header().Add("Content-Type", "applicaton/json")
 	w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(response); err != nil {
@@ -140,6 +153,191 @@ func (r *Router) HandleGetTodos(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func (r *Router) HandleUpdateTodo(w http.ResponseWriter, req *http.Request) {}
+// fetch a single Todo GET /api/v1/users/{username}/todos/{todo_id}
+func (r *Router) HandleGetTodo(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
 
-func (r *Router) HandledeleteTodo(w http.ResponseWriter, req *http.Request) {}
+	username := vars["username"]
+	todoIdStr := vars["todo_id"]
+
+	if username == "" || todoIdStr == "" {
+		log.Println("username or todo_id is not passed")
+		http.Error(w, "username or todo_id is not passed", http.StatusBadRequest)
+		return
+	}
+
+	// check if user exists
+	_, exists := schema.Database.Users[username]
+
+	if !exists {
+		log.Printf("username %v does not exist", username)
+		http.Error(w, "username does not exist", http.StatusBadRequest)
+		return
+	}
+
+	todos := schema.TodosDataBase.User[username].AllTodos
+	if !exists {
+		log.Printf("username %v does not exist", username)
+		http.Error(w, "user does not have a todo entry yet", http.StatusBadRequest)
+		return
+	}
+
+	todoId, err := strconv.Atoi(todoIdStr)
+	if err != nil {
+		log.Println("Invalid todo_id, must be an integer")
+		http.Error(w, "Invalid todo_id", http.StatusBadRequest)
+		return
+	}
+
+	var thatTodo schema.TodoSchema
+
+	for idx, td := range todos {
+		if td.ID == todoId {
+			thatTodo.ID = todoId
+			thatTodo.Completed = todos[idx].Completed
+			thatTodo.Todo = todos[idx].Todo
+			break
+		}
+	}
+
+	if thatTodo.ID == 0 && thatTodo.Todo == "" && !thatTodo.Completed {
+		log.Println("todo not found")
+		http.Error(w, "todo not found", http.StatusInternalServerError)
+		return
+	}
+
+	response := schema.TodoResponse{
+		Response: schema.Response{
+			Message:    "Todos retrieved successfully",
+			StatusCode: 200,
+		},
+		Data: thatTodo,
+	}
+
+	w.Header().Add("Content-Type", "applicaton/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		log.Println("error encoding JSON")
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+	}
+}
+
+// update a Todo PUT /api/v1/users/{username}/todos/{todo_id}
+func (r *Router) HandleUpdateTodo(w http.ResponseWriter, req *http.Request) {
+	if req.Header.Get("Content-Type") != "application/json" {
+		log.Println("Content-type must be application/json")
+		http.Error(w, "Wrong content-type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	// get username fromparams
+	vars := mux.Vars(req)
+
+	username := vars["username"]
+
+	if username == "" {
+		log.Println("username not provided")
+		http.Error(w, "username not provided", http.StatusBadRequest)
+		return
+	}
+
+	// decode payload
+	todoInput := schema.TodoSchema{}
+
+	if err := json.NewDecoder(req.Body).Decode(&todoInput); err != nil {
+		log.Println("invalid JSON")
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	userTodos, exists := schema.TodosDataBase.User[username]
+
+	if !exists {
+		log.Println("invalid JSON")
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	updated := false
+	for i, ts := range userTodos.AllTodos {
+		if ts.ID == todoInput.ID {
+			if todoInput.Completed {
+				userTodos.AllTodos[i].Completed = todoInput.Completed
+			}
+
+			if todoInput.Completed {
+				userTodos.AllTodos[i].Todo = todoInput.Todo
+			}
+			updated = true
+			break
+		}
+	}
+
+	if !updated {
+		log.Println("Todo not found")
+		http.Error(w, "Todo not found", http.StatusNotFound)
+		return
+	}
+
+	schema.TodosDataBase.User[username] = userTodos
+
+	response := schema.TodoResponse{
+		Response: schema.Response{
+			Message:    "Todos retrieved successfully",
+			StatusCode: 200,
+		},
+		Data: userTodos.AllTodos,
+	}
+
+	w.Header().Add("Content-Type", "applicaton/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		log.Println("error encoding JSON")
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+	}
+}
+
+// delete a single Todo DELETE /api/v1/users/{username}/todos/{todo_id}
+func (r *Router) HandledeleteTodo(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+
+	username := vars["username"]
+	todoIdStr := vars["todo_id"]
+
+	if username == "" || todoIdStr == "" {
+		log.Println("username or todo_id not provided")
+		http.Error(w, "username or todo_id not provided", http.StatusBadRequest)
+		return
+	}
+
+	todoId, err := strconv.Atoi(todoIdStr)
+	if err != nil {
+		log.Println("Invalid todo_id, must be an integer")
+		http.Error(w, "Invalid todo_id", http.StatusBadRequest)
+		return
+	}
+
+	userTodos, exists := schema.TodosDataBase.User[username]
+
+	if !exists {
+		log.Println("username does not exist")
+		http.Error(w, "username does not exist", http.StatusBadRequest)
+		return
+	}
+
+	for idx, td := range userTodos.AllTodos {
+		if td.ID == todoId {
+			userTodos.AllTodos = append(userTodos.AllTodos[:idx], userTodos.AllTodos[idx+1:]...)
+			break
+		}
+
+	}
+
+	schema.TodosDataBase.User[username] = userTodos
+
+	w.Header().Add("Content-Type", "applicaton/json")
+	w.WriteHeader(http.StatusAccepted)
+
+}
